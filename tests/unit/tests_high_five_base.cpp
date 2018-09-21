8aeee260b6e891a7bbf06a9db8789d279b0b5a1d
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-09-21 23:28:29 +0200

    Adding broken test

--- tests/unit/tests_high_five_base.cpp
+++ tests/unit/tests_high_five_base.cpp
@@ -1329,3 +1329,46 @@ void readWriteShuffleDeflateTest() {
 BOOST_AUTO_TEST_CASE_TEMPLATE(ReadWriteShuffleDeflate, T, numerical_test_types) {
     readWriteShuffleDeflateTest<T>();
 }
+
+// Broadcasting is supported
+BOOST_AUTO_TEST_CASE(ReadInBroadcastDims) {
+
+    const std::string FILE_NAME("h5_missmatch1_dset.h5");
+    const std::string DATASET_NAME("dset");
+
+    // Create a new file using the default property lists.
+    File file(FILE_NAME, File::ReadWrite | File::Create | File::Truncate);
+
+    // Create the data space for the dataset.
+    std::vector<size_t> dims_a{1,3};
+    std::vector<size_t> dims_b{3,1};
+
+    // 1D input / output vectors
+    std::vector<double> some_data{5.0, 6.0, 7.0};
+    std::vector<double> data_a;
+    std::vector<double> data_b;
+
+    DataSpace dataspace_a(dims_a);
+    DataSpace dataspace_b(dims_b);
+
+    // Create a dataset with double precision floating points
+    DataSet dataset_a = file.createDataSet(DATASET_NAME + "_a", dataspace_a, AtomicType<double>());
+    DataSet dataset_b = file.createDataSet(DATASET_NAME + "_b", dataspace_b, AtomicType<double>());
+
+    dataset_a.write(some_data);
+    dataset_b.write(some_data);
+
+    DataSet out_a = file.getDataSet(DATASET_NAME + "_a");
+    DataSet out_b = file.getDataSet(DATASET_NAME + "_b");
+
+    out_a.read(data_a);
+    out_b.read(data_b);
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(
+            data_a.begin(), data_a.end(),
+            some_data.begin(), some_data.end());
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(
+            data_b.begin(), data_b.end(),
+            some_data.begin(), some_data.end());
+}
