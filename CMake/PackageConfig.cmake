5291c0fb8b72300080691b007c08a2324d8b93af
Tristan Carel <tristan.carel@gmail.com>
2019-07-05 11:22:44 +0200

    Travis: add windows build
    Fixes #146

--- CMake/PackageConfig.cmake
+++ CMake/PackageConfig.cmake
@@ -8,6 +8,7 @@ target_include_directories(HighFive SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
 if(USE_BOOST)
   target_include_directories(HighFive SYSTEM INTERFACE ${Boost_INCLUDE_DIR})
   target_compile_definitions(HighFive INTERFACE -DH5_USE_BOOST)
+  target_link_libraries(HighFive INTERFACE Boost::serialization)
 endif()
 
 install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/highfive
