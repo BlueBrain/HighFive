3a0d20f3529794ae18e0e50daad1dd822bea993c
Fernando Pereira <fernando.pereira@epfl.ch>
2019-07-08 12:00:12 +0200

    StdChecks for older CMakes

--- CMake/portability/CheckCXXStandardSupport.cmake
+++ CMake/portability/CheckCXXStandardSupport.cmake
@@ -4,9 +4,17 @@
 
 include(CheckCXXCompilerFlag)
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "XL")
-    set(COMPILER_SUPPORTS_CXX11 FALSE)
-    return()
+# Set flags for older CMake
+if(${CMAKE_VERSION} VERSION_LESS 3.10)
+    if (CMAKE_CXX_COMPILER_ID MATCHES "(GNU|Clang|AppleClang|Intel|PGI|XL)")
+        set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "-std=c++11")
+        set(CMAKE_CXX14_STANDARD_COMPILE_OPTION "-std=c++14")
+        set(CMAKE_CXX17_STANDARD_COMPILE_OPTION "-std=c++17")
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+        set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "/std:c++11")
+        set(CMAKE_CXX14_STANDARD_COMPILE_OPTION "/std:c++14")
+        set(CMAKE_CXX17_STANDARD_COMPILE_OPTION "/std:c++17")
+    endif()
 endif()
 
 check_cxx_compiler_flag("${CMAKE_CXX11_STANDARD_COMPILE_OPTION}" COMPILER_SUPPORTS_CXX11)
