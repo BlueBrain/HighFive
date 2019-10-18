cae3dab4a3b7d795737c8879cd3141dbf41e8eb4
Nicolas Cornu <nicolas.cornu@epfl.ch>
2019-10-18 11:54:35 +0200

    Don't use MPI::MPI_C target
    This target is only available in really new version of cmake

--- CMake/PackageConfig.cmake
+++ CMake/PackageConfig.cmake
@@ -10,7 +10,11 @@ target_compile_definitions(HighFive INTERFACE ${HDF5_DEFINITIONS})
 
 # MPI
 if(HIGHFIVE_PARALLEL_HDF5)
-  target_link_libraries(HighFive INTERFACE MPI::MPI_C)
+  target_include_directories(HighFive SYSTEM INTERFACE ${MPI_C_INCLUDE_PATH})
+  target_link_libraries(HighFive INTERFACE ${MPI_C_LIBRARIES})
+  if(CMAKE_VERSION VERSION_GREATER 3.13)
+    target_link_options(HighFive INTERFACE ${MPI_C_LINK_FLAGS})
+  endif()
 endif()
 
 # BOOST
