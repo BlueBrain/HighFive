0c9005c9e9532101856a96cb0ea4e6eb97b3ba8a
Nicolas Cornu <nicolas.cornu@epfl.ch>
2019-10-18 16:47:39 +0200

    Revert "Use set_target_properties instead of target_link_options"
    This reverts commit 056f78e9a9ecb42469c18591500ea972105cec5c.

--- CMake/PackageConfig.cmake
+++ CMake/PackageConfig.cmake
@@ -12,7 +12,9 @@ target_compile_definitions(HighFive INTERFACE ${HDF5_DEFINITIONS})
 if(HIGHFIVE_PARALLEL_HDF5)
   target_include_directories(HighFive SYSTEM INTERFACE ${MPI_C_INCLUDE_PATH})
   target_link_libraries(HighFive INTERFACE ${MPI_C_LIBRARIES})
-  set_target_properties(HighFive PROPERTIES LINK_FLAGS ${MPI_C_LINK_FLAGS})
+  if(CMAKE_VERSION VERSION_GREATER 3.13)
+    target_link_options(HighFive INTERFACE ${MPI_C_LINK_FLAGS})
+  endif()
 endif()
 
 # BOOST
