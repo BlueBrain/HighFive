d211d89d2a25b67a8f0a44e52e4be7bf4f5e714d
Fernando Pereira <fernando.pereira@epfl.ch>
2019-12-05 14:07:45 +0100

    All flags from target. Same for boost. Cleanup

--- CMake/PackageConfig.cmake
+++ CMake/PackageConfig.cmake
@@ -5,44 +5,36 @@ target_include_directories(HighFive INTERFACE
   "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>"
   "$<INSTALL_INTERFACE:include>")
 
-target_include_directories(HighFive SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
-target_link_libraries(HighFive INTERFACE ${HDF5_C_LIBRARIES})
-target_compile_definitions(HighFive INTERFACE ${HDF5_DEFINITIONS})
-
-# MPI
-if(HIGHFIVE_PARALLEL_HDF5 OR HDF5_IS_PARALLEL)
-  target_include_directories(HighFive SYSTEM INTERFACE ${MPI_CXX_INCLUDE_PATH})
-  target_link_libraries(HighFive INTERFACE ${MPI_CXX_LIBRARIES})
-  if(CMAKE_VERSION VERSION_LESS 3.13)
-    target_link_libraries(HighFive INTERFACE ${MPI_CXX_LINK_FLAGS})
-  else()
-    target_link_options(HighFive INTERFACE "SHELL:${MPI_CXX_LINK_FLAGS}")
+# HDF5
+foreach(target_name "${HDF5_C_TARGET}" "${HDF5_C_TARGET}-shared" "${HDF5_C_TARGET}-static")
+  if(TARGET "${target_name}")
+    target_link_libraries(HighFive INTERFACE "${target_name}")
+    break()
   endif()
-endif()
+endforeach()
 
 # BOOST
 if(USE_BOOST)
-  target_include_directories(HighFive SYSTEM INTERFACE ${Boost_INCLUDE_DIR})
+  target_link_libraries(HighFive INTERFACE Boost::boost Boost::serialization)
   target_compile_definitions(HighFive INTERFACE -DH5_USE_BOOST)
-  target_link_libraries(HighFive INTERFACE ${Boost_SERIALIZATION_LIBRARIES})
 endif()
 
-install(DIRECTORY include/
-  DESTINATION "include")
+
+# Generate ${PROJECT_NAME}Config.cmake
 
 include(CMakePackageConfigHelpers)
 configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/HighFiveConfig.cmake.in
-  ${PROJECT_BINARY_DIR}/HighFiveConfig.cmake
+  ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
   INSTALL_DESTINATION share/${PROJECT_NAME}/CMake)
 
-include(CMakePackageConfigHelpers)
 write_basic_package_version_file(
     ${PROJECT_NAME}ConfigVersion.cmake
     VERSION ${PROJECT_VERSION}
     COMPATIBILITY AnyNewerVersion)
 
-install(FILES ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
-  ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+install(FILES
+    ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+    ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
   DESTINATION share/${PROJECT_NAME}/CMake)
 
 
@@ -63,6 +55,6 @@ install(EXPORT HighFiveTargets
         FILE ${PROJECT_NAME}Targets.cmake
         DESTINATION share/${PROJECT_NAME}/CMake)
 
-# Generate the Export for the BUILD_INTERACE
+# Generate the Export for the BUILD_INTERACE (hardly used)
 export(EXPORT HighFiveTargets
        FILE "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Targets.cmake")
