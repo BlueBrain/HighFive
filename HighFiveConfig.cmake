ae9c4b205eb9d0f88321663b8627f09f9c73c317
Tom de Geus <tom@geus.me>
2019-12-05 08:59:44 +0100

    First working version

--- HighFiveConfig.cmake
+++ HighFiveConfig.cmake
@@ -1,8 +1,18 @@
+
 get_filename_component(HighFive_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+
 include(CMakeFindDependencyMacro)
 
-find_dependency(Boost QUIET)
-find_dependency(xtensor QUIET)
+
+if(NOT TARGET HighFive::HDF5)
+    find_package(HDF5 REQUIRED)
+    add_library(HighFive::HDF5 INTERFACE IMPORTED)
+    set_target_properties(HighFive::HDF5 PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${HDF5_INCLUDE_DIRS}"
+        INTERFACE_LINK_LIBRARIES "${HDF5_LIBRARIES}")
+endif()
+
 
 if(NOT TARGET HighFive::HighFive)
     include("${HighFive_CMAKE_DIR}/HighFiveTargets.cmake")
