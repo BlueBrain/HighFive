da5c93e93a688a5e9cf5f2ec0df640f29a5c2ca0
Tom de Geus <tom@geus.me>
2019-12-05 17:49:31 +0100

    Enabling tests and examples

--- HighFiveConfig.cmake
+++ HighFiveConfig.cmake
@@ -1,47 +1,102 @@
 
 get_filename_component(HighFive_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
-
 include(CMakeFindDependencyMacro)
 
+# ---------------------------
+# Set target "HighFive::HDF5"
+# ---------------------------
 
 if(NOT TARGET HighFive::HDF5)
+
     find_package(HDF5 REQUIRED)
+
     add_library(HighFive::HDF5 INTERFACE IMPORTED)
+
     set_target_properties(HighFive::HDF5 PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${HDF5_INCLUDE_DIRS}"
         INTERFACE_LINK_LIBRARIES "${HDF5_LIBRARIES}")
+
 endif()
 
+# ----------------------------
+# Set target "HighFive::Boost"
+# ----------------------------
 
 if(NOT TARGET HighFive::Boost)
-    find_package(Boost QUIET)
+
     add_library(HighFive::Boost INTERFACE IMPORTED)
-    if(Boost_FOUND)
-        target_link_libraries(HighFive::Boost INTERFACE Boost::boost)
-        target_compile_definitions(HighFive::Boost INTERFACE "H5_USE_BOOST")
+
+    if(NOT HIGHFIVE_NO_BOOST)
+
+        find_package(Boost COMPONENTS system serialization QUIET)
+
+        if(Boost_FOUND)
+            target_link_libraries(HighFive::Boost INTERFACE
+                Boost::boost
+                Boost::system
+                Boost::serialization)
+            target_compile_definitions(HighFive::Boost INTERFACE "H5_USE_BOOST")
+        endif()
+
     endif()
+
 endif()
 
+# ------------------------------
+# Set target "HighFive::xtensor"
+# ------------------------------
 
 if(NOT TARGET HighFive::xtensor)
-    find_package(xtensor QUIET)
+
     add_library(HighFive::xtensor INTERFACE IMPORTED)
-    if(xtensor_FOUND)
-        target_link_libraries(HighFive::xtensor INTERFACE xtensor)
-        target_compile_definitions(HighFive::xtensor INTERFACE "H5_USE_XTENSOR")
+
+    if(NOT HIGHFIVE_NO_XTENSOR)
+
+        find_package(xtensor QUIET)
+
+        if(xtensor_FOUND)
+            target_link_libraries(HighFive::xtensor INTERFACE xtensor)
+            target_compile_definitions(HighFive::xtensor INTERFACE "H5_USE_XTENSOR")
+        endif()
+
     endif()
+
 endif()
 
+# ----------------------------
+# Set target "HighFive::Eigen"
+# ----------------------------
 
 if(NOT TARGET HighFive::Eigen)
-    find_package(Eigen3 QUIET)
+
     add_library(HighFive::Eigen INTERFACE IMPORTED)
-    set_target_properties(HighFive::Eigen PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIR}")
+
+    if(NOT HIGHFIVE_NO_EIGEN)
+
+        find_package(Eigen3 QUIET)
+
+        if(NOT Eigen3_FOUND)
+
+            find_package(PkgConfig QUIET)
+
+            if (PkgConfig_FOUND)
+                pkg_check_modules(EIGEN3 QUIET eigen3)
+            endif()
+
+        endif()
+
+        set_target_properties(HighFive::Eigen PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIR}")
+
+    endif()
+
 endif()
 
+# ---------------------
+# Set target "HighFive"
+# ---------------------
 
-if(NOT TARGET HighFive)
+if(NOT TARGET HighFive AND NOT HIGHFIVE_NO_TARGET)
     include("${HighFive_CMAKE_DIR}/HighFiveTargets.cmake")
 endif()
