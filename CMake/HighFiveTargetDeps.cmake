a914790f0119982878388f15a18bcb686c4a8812
Tom de Geus <tom@geus.me>
2019-12-12 09:07:14 +0100

    Updating based on review

--- CMake/HighFiveTargetDeps.cmake
+++ CMake/HighFiveTargetDeps.cmake
@@ -1,24 +1,24 @@
 
 if(TARGET highfive_deps)
-    return()
+  return()
 endif()
 
 if (HIGHFIVE_USE_INSTALL_DEPS)
-    add_library(highfive_deps INTERFACE)
+  add_library(highfive_deps INTERFACE)
 else()
-    add_library(highfive_deps INTERFACE IMPORTED)
+  add_library(highfive_deps INTERFACE IMPORTED)
 endif()
 
 # HDF5
 
 if(NOT DEFINED HDF5_C_LIBRARIES)
-    set(HDF5_NO_FIND_PACKAGE_CONFIG_FILE TRUE)  # Consistency
-    set(HDF5_PREFER_PARALLEL ${HIGHFIVE_PARALLEL_HDF5})
-    find_package(HDF5 REQUIRED)
+  set(HDF5_NO_FIND_PACKAGE_CONFIG_FILE TRUE)  # Consistency
+  set(HDF5_PREFER_PARALLEL ${HIGHFIVE_PARALLEL_HDF5})
+  find_package(HDF5 REQUIRED)
 endif()
 
 if(HIGHFIVE_PARALLEL_HDF5 AND NOT HDF5_IS_PARALLEL)
-    message(WARNING "Parallel HDF5 requested but libhdf5 doesn't support it")
+  message(WARNING "Parallel HDF5 requested but libhdf5 doesn't support it")
 endif()
 
 target_include_directories(highfive_deps SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
@@ -28,32 +28,29 @@ target_compile_definitions(highfive_deps INTERFACE ${HDF5_DEFINITIONS})
 # Boost
 
 if(HIGHFIVE_USE_BOOST)
-    set(Boost_NO_BOOST_CMAKE TRUE)  # Consistency
-    find_package(Boost REQUIRED COMPONENTS system serialization)
-    if(${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.5)
-        target_link_libraries(highfive_deps INTERFACE
-                Boost::boost
-                Boost::system
-                Boost::serialization)
-    else()
-        target_include_directories(highfive_deps SYSTEM INTERFACE ${Boost_INCLUDE_DIR})
-    endif()
-    target_compile_definitions(highfive_deps INTERFACE BOOST_ALL_NO_LIB H5_USE_BOOST)
+  set(Boost_NO_BOOST_CMAKE TRUE)  # Consistency
+  find_package(Boost REQUIRED COMPONENTS system serialization)
+  if(${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.5)
+    target_link_libraries(highfive_deps INTERFACE Boost::boost Boost::system Boost::serialization)
+  else()
+    target_include_directories(highfive_deps SYSTEM INTERFACE ${Boost_INCLUDE_DIR})
+  endif()
+  target_compile_definitions(highfive_deps INTERFACE BOOST_ALL_NO_LIB H5_USE_BOOST)
 endif()
 
 # MPI
 
 if(HIGHFIVE_PARALLEL_HDF5 OR HDF5_IS_PARALLEL)
-    find_package(MPI REQUIRED)
-    target_include_directories(highfive_deps SYSTEM INTERFACE ${MPI_CXX_INCLUDE_PATH})
-    target_link_libraries(highfive_deps INTERFACE ${MPI_CXX_LIBRARIES})
-    if(CMAKE_VERSION VERSION_LESS 3.13)
-        target_link_libraries(highfive_deps INTERFACE ${MPI_CXX_LINK_FLAGS})
-    else()
-        target_link_options(highfive_deps INTERFACE "SHELL:${MPI_CXX_LINK_FLAGS}")
-    endif()
+  find_package(MPI REQUIRED)
+  target_include_directories(highfive_deps SYSTEM INTERFACE ${MPI_CXX_INCLUDE_PATH})
+  target_link_libraries(highfive_deps INTERFACE ${MPI_CXX_LIBRARIES})
+  if(CMAKE_VERSION VERSION_LESS 3.13)
+    target_link_libraries(highfive_deps INTERFACE ${MPI_CXX_LINK_FLAGS})
+  else()
+    target_link_options(highfive_deps INTERFACE "SHELL:${MPI_CXX_LINK_FLAGS}")
+  endif()
 endif()
 
 if(CMAKE_VERSION VERSION_LESS "3.1")
-    message(WARNING "HighFive requires at least c++11. You may need to set CMAKE_CXX_STANDARD.")
+  message(WARNING "HighFive requires at least c++11. You may need to set CMAKE_CXX_STANDARD.")
 endif()
--- CMake/HighFiveTargetExport.cmake
+++ CMake/HighFiveTargetExport.cmake
@@ -2,7 +2,8 @@ add_library(HighFive INTERFACE)
 
 # Public headers
 
-target_include_directories(HighFive INTERFACE
+target_include_directories(HighFive
+  INTERFACE
     "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>"
     "$<INSTALL_INTERFACE:include>")
 
@@ -14,11 +15,11 @@ target_compile_definitions(HighFive INTERFACE MPI_NO_CPPBIND)  # No c++ bindings
 # Ensure we activate at least C++11
 
 if(NOT DEFINED CMAKE_CXX_STANDARD)
-    if(CMAKE_VERSION VERSION_LESS "3.1")
-        message(WARNING "HighFive requires at least c++11. You may need to set CMAKE_CXX_STANDARD.")
-    else()
-        target_compile_features(HighFive INTERFACE cxx_std_11)
-    endif()
+  if(CMAKE_VERSION VERSION_LESS "3.1")
+    message(WARNING "HighFive requires at least c++11. You may need to set CMAKE_CXX_STANDARD.")
+  else()
+    target_compile_features(HighFive INTERFACE cxx_std_11)
+  endif()
 endif()
 
 # Generate HighFiveConfig.cmake
@@ -26,75 +27,75 @@ endif()
 include(CMakePackageConfigHelpers)
 
 configure_package_config_file(
-        ${CMAKE_CURRENT_LIST_DIR}/HighFiveConfig.cmake.in
-        ${PROJECT_BINARY_DIR}/HighFiveConfig.cmake
-    INSTALL_DESTINATION
-        share/HighFive/CMake)
+    ${CMAKE_CURRENT_LIST_DIR}/HighFiveConfig.cmake.in
+    ${PROJECT_BINARY_DIR}/HighFiveConfig.cmake
+  INSTALL_DESTINATION
+    share/HighFive/CMake)
 
 write_basic_package_version_file(
-        HighFiveConfigVersion.cmake
-    VERSION
-        ${PROJECT_VERSION}
-    COMPATIBILITY
-        AnyNewerVersion)
+    HighFiveConfigVersion.cmake
+  VERSION
+    ${PROJECT_VERSION}
+  COMPATIBILITY
+    AnyNewerVersion)
 
 install(
-    FILES
-        CMake/HighFiveTargetDeps.cmake
-        ${PROJECT_BINARY_DIR}/HighFiveConfig.cmake
-        ${PROJECT_BINARY_DIR}/HighFiveConfigVersion.cmake
-    DESTINATION
-        share/HighFive/CMake)
+  FILES
+    CMake/HighFiveTargetDeps.cmake
+    ${PROJECT_BINARY_DIR}/HighFiveConfig.cmake
+    ${PROJECT_BINARY_DIR}/HighFiveConfigVersion.cmake
+  DESTINATION
+    share/HighFive/CMake)
 
 # Generate HighFiveTargets.cmake
 
 install(
-    TARGETS
-        HighFive
-    EXPORT
-        HighFiveTargets
-    INCLUDES DESTINATION
-        include)
+  TARGETS
+    HighFive
+  EXPORT
+    HighFiveTargets
+  INCLUDES DESTINATION
+    include)
 
 install(
-    EXPORT
-        HighFiveTargets
-    FILE
-        HighFiveTargets.cmake
-    DESTINATION
-        share/HighFive/CMake)
+  EXPORT
+    HighFiveTargets
+  FILE
+    HighFiveTargets.cmake
+  DESTINATION
+    share/HighFive/CMake)
 
 export(
-    EXPORT
-        HighFiveTargets
-    FILE
-        "${PROJECT_BINARY_DIR}/HighFiveTargets.cmake")
+  EXPORT
+    HighFiveTargets
+  FILE
+    "${PROJECT_BINARY_DIR}/HighFiveTargets.cmake")
 
 # Install static target "highfive_deps" if requested
 # If not installed it is constructed when the user calls "find_package(HighFive)"
 
-if (HIGHFIVE_USE_INSTALL_DEPS)
+if (HIGHFIVE_INSTALL_STATIC)
 
-    install(
-        TARGETS
-            highfive_deps
-        EXPORT
-            HighFiveStaticTargets
-        INCLUDES DESTINATION
-            include)
+  install(
+    TARGETS
+      highfive_deps
+    EXPORT
+      HighFiveStaticTargets
+    INCLUDES DESTINATION
+      include)
 
-    install(
-        EXPORT
-            HighFiveStaticTargets
-        FILE
-            HighFiveStaticTargets.cmake
-        DESTINATION
-            share/HighFive/CMake)
+  install(
+    EXPORT
+      HighFiveStaticTargets
+    FILE
+      HighFiveStaticTargets.cmake
+    DESTINATION
+      share/HighFive/CMake)
 
-    export(
-        EXPORT
-            HighFiveStaticTargets
-        FILE
-            "${PROJECT_BINARY_DIR}/HighFiveStaticTargets.cmake")
+  export(
+    EXPORT
+      HighFiveStaticTargets
+    FILE
+      "${PROJECT_BINARY_DIR}/HighFiveStaticTargets.cmake")
 
 endif()
