90c70d007a1fc377427b96defd27da40e3fbbb4f
Fernando Pereira <ferdonline@gmail.com>
2020-03-01 18:30:28 +0100

    CMake compat with v3.1

--- CMake/HighFiveTargetDeps.cmake
+++ CMake/HighFiveTargetDeps.cmake
@@ -1,21 +1,8 @@
 # Link against target system libs
 # -------------------------------
 
-# therefore making it possible to have new dependencies each build
-if(NOT TARGET highfive_deps)
-  add_library(highfive_deps INTERFACE)
-else()
-# Reset if imported
-set_target_properties(highfive_deps PROPERTIES
-  INTERFACE_COMPILE_DEFINITIONS ""
-  INTERFACE_COMPILE_OPTIONS ""
-  INTERFACE_INCLUDE_DIRECTORIES ""
-  INTERFACE_LINK_LIBRARIES ""
-  INTERFACE_LINK_OPTIONS ""
-  INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ""
-)
-endif()
-
+# Independent target to make it possible to have new dependencies each build
+add_library(_libdeps INTERFACE)
 
 # HDF5
 if(NOT DEFINED HDF5_C_LIBRARIES)
@@ -28,40 +15,28 @@ if(HIGHFIVE_PARALLEL_HDF5 AND NOT HDF5_IS_PARALLEL)
   message(WARNING "Parallel HDF5 requested but libhdf5 doesnt support it")
 endif()
 
-target_include_directories(highfive_deps SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
-target_link_libraries(highfive_deps INTERFACE ${HDF5_C_LIBRARIES})
-target_compile_definitions(highfive_deps INTERFACE ${HDF5_DEFINITIONS})
+target_include_directories(_libdeps SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
+target_link_libraries(_libdeps INTERFACE ${HDF5_C_LIBRARIES})
+target_compile_definitions(_libdeps INTERFACE ${HDF5_DEFINITIONS})
 
 # Boost
 if(HIGHFIVE_USE_BOOST)
   set(Boost_NO_BOOST_CMAKE TRUE)  # Consistency
   find_package(Boost REQUIRED COMPONENTS system serialization)
   # Dont use imported targets yet, not avail before cmake 3.5
-  target_include_directories(highfive_deps SYSTEM INTERFACE ${Boost_INCLUDE_DIR})
-  target_compile_definitions(highfive_deps INTERFACE BOOST_ALL_NO_LIB H5_USE_BOOST)
+  target_include_directories(_libdeps SYSTEM INTERFACE ${Boost_INCLUDE_DIR})
+  target_compile_definitions(_libdeps INTERFACE BOOST_ALL_NO_LIB H5_USE_BOOST)
 endif()
 
 # MPI
 if(HIGHFIVE_PARALLEL_HDF5 OR HDF5_IS_PARALLEL)
   find_package(MPI REQUIRED)
-  target_include_directories(highfive_deps SYSTEM INTERFACE ${MPI_CXX_INCLUDE_PATH})
-  target_link_libraries(highfive_deps INTERFACE ${MPI_CXX_LIBRARIES})
+  target_include_directories(_libdeps SYSTEM INTERFACE ${MPI_CXX_INCLUDE_PATH})
+  target_link_libraries(_libdeps INTERFACE ${MPI_CXX_LIBRARIES})
   if(CMAKE_VERSION VERSION_LESS 3.13)
-    target_link_libraries(highfive_deps INTERFACE ${MPI_CXX_LINK_FLAGS})
+    target_link_libraries(_libdeps INTERFACE ${MPI_CXX_LINK_FLAGS})
   else()
-    target_link_options(highfive_deps INTERFACE "SHELL:${MPI_CXX_LINK_FLAGS}")
+    target_link_options(_libdeps INTERFACE "SHELL:${MPI_CXX_LINK_FLAGS}")
   endif()
 endif()
 
-# Propagate to HighFive
-target_link_libraries(HighFive INTERFACE highfive_deps)
-target_compile_definitions(HighFive INTERFACE MPI_NO_CPPBIND)  # No c++ bindings
-
-# Ensure we activate at least C++11
-if(NOT DEFINED CMAKE_CXX_STANDARD)
-  if(CMAKE_VERSION VERSION_LESS "3.1")
-    message(WARNING "HighFive requires at least c++11. You may need to set CMAKE_CXX_STANDARD.")
-  else()
-    target_compile_features(HighFive INTERFACE cxx_std_11)
-  endif()
-endif()
--- CMake/HighFiveTargetExport.cmake
+++ CMake/HighFiveTargetExport.cmake
@@ -1,10 +1,15 @@
-add_library(HighFive INTERFACE)
-add_library(highfive_deps INTERFACE)
 
-# Public headers
-target_include_directories(HighFive INTERFACE
+# Define the HighFive INTERFACE library
+add_library(_libheaders INTERFACE)
+
+target_include_directories(_libheaders INTERFACE
   "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>"
   "$<INSTALL_INTERFACE:include>")
+
+# Combined HighFive
+add_library(HighFive INTERFACE)
+target_compile_definitions(HighFive INTERFACE MPI_NO_CPPBIND)  # No c++ bindings
+target_link_libraries(HighFive INTERFACE _libheaders _libdeps)
 
 
 # Generate ${PROJECT_NAME}Config.cmake
@@ -26,23 +31,18 @@ install(FILES
   DESTINATION share/${PROJECT_NAME}/CMake)
 
 
-# Generate ${PROJECT_NAME}Targets.cmake;
 # Provides IMPORTED targets when using this project from build/install trees.
 
-# NOTE: the export file is ${PROJECT_NAME}Targets to support when HighFive
-# is built within a 3rd-party project (X). Other projects can find and import
-# X-Targets.cmake containing the HighFive target
-
-# Specify targets to include in the HighFive Export
-install(TARGETS HighFive highfive_deps
-        EXPORT HighFiveTargets
-        INCLUDES DESTINATION include)
+# Specify targets to include in the HighFive Exports
+install(TARGETS _libheaders _libdeps
+        EXPORT HighFiveTargets)
 
 # Generate & install the Export for the INSTALL_INTERFACE
 install(EXPORT HighFiveTargets
-        FILE ${PROJECT_NAME}Targets.cmake
+        NAMESPACE HighFive
+        FILE HighFiveTargets.cmake
         DESTINATION share/${PROJECT_NAME}/CMake)
 
 # Generate the Export for the BUILD_INTERACE (hardly used)
 export(EXPORT HighFiveTargets
-       FILE "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Targets.cmake")
+       FILE "${PROJECT_BINARY_DIR}/HighFiveTargets.cmake")
