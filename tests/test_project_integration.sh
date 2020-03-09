b7342c701b5295d73c34367334b1dfdf547cc26f
Matthias Wolf <matthias.wolf@epfl.ch>
2020-03-09 11:25:02 +0100

    Rework integration script.

--- tests/test_project_integration.sh
+++ tests/test_project_integration.sh
@@ -2,44 +2,48 @@
 set -xeo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}")"  # cd here
 
-rm -rf _highfive_build
-mkdir -p _highfive_build
-pushd _highfive_build
-cmake ../.. -DHIGHFIVE_EXAMPLES=OFF -DHIGHFIVE_UNIT_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PWD/install
+BUILDDIR="${PWD}/build/highfive"
+ROOT="${PWD}/.."
+TESTDIR="${PWD}"
+
+test_install() {
+    local project="${1}"
+    local builddir="${BUILDDIR}/${project}/${2}"
+    shift
+    shift
+    rm -rf "${builddir}"
+    mkdir -p "${builddir}"
+    pushd "${builddir}"
+    cmake "${TESTDIR}/${project}" $*
+    make VERBOSE=1
+    ctest
+    popd
+}
+
+rm -rf "${BUILDDIR}/highfive"
+mkdir -p "${BUILDDIR}/highfive"
+pushd "${BUILDDIR}/highfive"
+cmake "${ROOT}" \
+    -DHIGHFIVE_EXAMPLES=OFF \
+    -DHIGHFIVE_UNIT_TESTS=OFF \
+    -DCMAKE_INSTALL_PREFIX="${PWD}/install"
 make install
 popd
 
 for project in test_project test_library; do
     # Case 1. Base case: include subdirectory
-    rm -rf ${project}/_build_subdir
-    mkdir -p ${project}/_build_subdir
-    pushd ${project}/_build_subdir
-    cmake ..
-    make VERBOSE=1
-    ctest
-    popd
+    test_install "${project}" subdir
 
     # Case 2. We use an install dir and all deps configuration
     # Install highfive (no tests required)
-
-    rm -rf ${project}/_build_install_reuse_deps
-    mkdir -p ${project}/_build_install_reuse_deps
-    pushd ${project}/_build_install_reuse_deps
-    cmake .. -DUSE_BUNDLED_HIGHFIVE=NO \
+    test_install "${project}" reuse_deps \
+        -DUSE_BUNDLED_HIGHFIVE=NO \
         -DHIGHFIVE_USE_INSTALL_DEPS=YES \
-        -DCMAKE_PREFIX_PATH=$PWD/../../_highfive_build/install
-    make VERBOSE=1
-    ctest
-    popd
-
+        -DCMAKE_PREFIX_PATH="${BUILDDIR}/highfive/install"
+            #
     # Case 3. We redetect-dependencies
-    rm -rf ${project}/_build_install_new_deps
-    mkdir -p ${project}/_build_install_new_deps
-    pushd ${project}/_build_install_new_deps
-    cmake .. -DUSE_BUNDLED_HIGHFIVE=NO \
+    test_install "${project}" install_new_deps \
+        -DUSE_BUNDLED_HIGHFIVE=NO \
         -DHIGHFIVE_USE_INSTALL_DEPS=NO \
-        -DCMAKE_PREFIX_PATH=$PWD/../../_highfive_build/install
-    make VERBOSE=1
-    ctest
-    popd
+        -DCMAKE_PREFIX_PATH="${BUILDDIR}/highfive/install"
 done
