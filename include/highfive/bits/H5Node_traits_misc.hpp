10b2431aace64750bd0f0a004daff0d38e22afca
Kerim <kerim.khemraev@mail.ru>
2020-05-31 14:17:57 +0300

    Update include/highfive/bits/H5Node_traits_misc.hpp
    Co-authored-by: Tom de Geus <tdegeus@users.noreply.github.com>

--- include/highfive/bits/H5Node_traits_misc.hpp
+++ include/highfive/bits/H5Node_traits_misc.hpp
@@ -142,7 +142,7 @@ inline size_t NodeTraits<Derivate>::getNumberObjects() const {
 }
 
 template <typename Derivate>
-inline std::string NodeTraits<Derivate>::getName(size_t index) const {
+inline std::string NodeTraits<Derivate>::getObjectName(size_t index) const {
     const size_t maxLength = 255;
     char buffer[maxLength + 1];
     ssize_t retcode = H5Lget_name_by_idx(
