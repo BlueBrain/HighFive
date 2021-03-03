fb80d273790a2c438c86d7759754bc124a642d0d
Nicolas Cornu <nicolac76@yahoo.fr>
2021-03-03 14:05:29 +0100

    Clean commit

--- CMake/HighFiveTargetExport.cmake
+++ CMake/HighFiveTargetExport.cmake
@@ -8,8 +8,9 @@ target_include_directories(libheaders INTERFACE
 
 # Combined HighFive
 add_library(HighFive INTERFACE)
-target_compile_definitions(HighFive INTERFACE MPI_NO_CPPBIND) # No c++ bindings
+target_compile_definitions(HighFive INTERFACE MPI_NO_CPPBIND)  # No c++ bindings
 target_link_libraries(HighFive INTERFACE libheaders libdeps)
+
 
 # Generate ${PROJECT_NAME}Config.cmake
 
