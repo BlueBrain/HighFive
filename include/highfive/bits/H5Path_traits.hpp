99a41996b2c82d4ac00d5a9ceb1fc28bf9c4e8a3
Fernando Pereira <fernando.pereira@epfl.ch>
2021-03-11 17:16:43 +0100

    Fix copyright notice and style

--- include/highfive/bits/H5Path_traits.hpp
+++ include/highfive/bits/H5Path_traits.hpp
@@ -1,8 +1,14 @@
-
+/*
+ *  Copyright (c), 2020, EPFL - Blue Brain Project
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ *
+ */
 #pragma once
 
 #include "H5_definitions.hpp"
-
 
 namespace HighFive {
 
--- include/highfive/bits/H5Path_traits_misc.hpp
+++ include/highfive/bits/H5Path_traits_misc.hpp
@@ -1,3 +1,13 @@
+/*
+ *  Copyright (c), 2020, EPFL - Blue Brain Project
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ *
+ */
+#pragma once
+
 #include <H5Ipublic.h>
 
 #include "H5Utils.hpp"
