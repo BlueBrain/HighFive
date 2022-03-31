0f53961a30e9984fd80a6c954374e2e6f4a083e2
Matthias Wolf <matthias.wolf@epfl.ch>
2022-03-31 15:43:17 +0200

    Build documentation for PRs, publish on push to master

--- .github/build.sh
+++ .github/build.sh
@@ -3,14 +3,9 @@
 [ "$CC" ] && $CC --version
 cmake --version
 set -x
-cmake -E make_directory $GITHUB_WORKSPACE/build
-cd $GITHUB_WORKSPACE/build
-cmake $GITHUB_WORKSPACE \
+cmake -B $GITHUB_WORKSPACE/build -S $GITHUB_WORKSPACE \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
   -DHIGHFIVE_USE_EIGEN:BOOL=TRUE \
   "${CMAKE_OPTIONS[@]}"
-
-
-echo ">>> BUILDING"
-cmake --build . --config $BUILD_TYPE --parallel 2 --verbose
+cmake --build $GITHUB_WORKSPACE/build --config $BUILD_TYPE --parallel 2 --verbose
