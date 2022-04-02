7f964f5319512dfbea9fd180fb0a111aa227d244
Fernando Pereira <fernando.pereira@epfl.ch>
2022-04-02 10:11:11 +0200

    Benchmark demos fixed

--- benchmarks/hdf5_bench.cpp
+++ benchmarks/hdf5_bench.cpp
@@ -1,44 +1,52 @@
+#include <iostream>
 #include <stdexcept>
 #include <vector>
 #include "hdf5.h"
 
 #define NROWS 1000000  // 1M
-#define ROW_VALUES {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+#define ROW_LENGTH 10
 
-
-const std::vector<std::vector<int>> data(NROWS, ROW_VALUES);
+const std::vector<std::vector<int>> data(NROWS, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
 
 int main() {
-    hid_t   file_id, dataset_id, dataspace_id; /* identifiers */
-    hsize_t dims[2];
-    herr_t  status;
-
     /* Create a new file using default properties. */
-    file_id = H5Fcreate("dataset_integer_raw.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    hid_t file_id = H5Fcreate("dataset_integer_raw.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
     /* Create the data space for the dataset. */
-    dims[0]      = NROWS;
-    dims[1]      = data.size();
-    dataspace_id = H5Screate_simple(2, dims, NULL);
+    hsize_t dims[] = {NROWS, ROW_LENGTH};
+    hid_t dataspace_id = H5Screate_simple(2, dims, NULL);
+
+    // Row memspace
+    hsize_t mem_dims[] = {1, ROW_LENGTH};
+    hid_t memspace_id = H5Screate_simple(2, mem_dims, NULL);
 
     /* Create the dataset. */
-    dataset_id =
-        H5Dcreate2(file_id, "/dataset", H5T_STD_I32BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    hid_t dataset_id =
+        H5Dcreate2(file_id, "/dataset", H5T_NATIVE_INT, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
-    /* Write to the dataset. */
-    for (const auto& row : data) {
-        status = H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, row.data());
-        if (status > 0) {
+    herr_t status;
+
+    /* Write each row to the dataset. */
+    for (size_t i=0; i<NROWS; i++) {
+        // File Hyperslabs
+        hsize_t count[] = {1, 10};
+        hsize_t offset[] = {i, 0};
+        status = H5Sselect_hyperslab(dataspace_id, H5S_SELECT_SET, offset, NULL, count, NULL);
+        if (status != 0) {
+            throw(std::runtime_error("H5Sselect_hyperslab failed"));
+        }
+        status = H5Dwrite(dataset_id, H5T_NATIVE_INT, memspace_id, dataspace_id, H5P_DEFAULT, data[i].data());
+        if (status != 0) {
             throw(std::runtime_error("H5Dwrite failed"));
         }
     }
 
-    /* End access to the dataset and release resources used by it. */
-    status = H5Dclose(dataset_id);
-
-    /* Terminate access to the data space. */
-    status = H5Sclose(dataspace_id);
-
-    /* Close the file. */
-    status = H5Fclose(file_id);
+    status = H5Sclose(memspace_id);
+    status |= H5Sclose(dataspace_id);
+    status |= H5Dclose(dataset_id);
+    status |= H5Fclose(file_id);
+    if (status != 0) {
+        std::cerr << "Error while releasing resources" << std::endl;
+    }
+    return status;
 }
