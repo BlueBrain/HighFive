c8d5721c45c082df02afbbe87a64fd52f5ea3339
Matthias Wolf <matthias.wolf@epfl.ch>
2023-03-24 14:39:14 +0100

    Allow HIGHFIVE_USE_INSTALL_DEPS only at install time
    Remove the "flexibility" of finding dependencies at use time. This
    simplifies our CMake code greatly.

--- CMake/HighFiveTargetDeps.cmake
+++ CMake/HighFiveTargetDeps.cmake
@@ -1,115 +1,108 @@
 # Link against target system libs
 # -------------------------------
 
-if(NOT TARGET libdeps)
+if(HIGHFIVE_VERBOSE)
+target_compile_definitions(HighFive INTERFACE -DHIGHFIVE_LOG_LEVEL=0)
+endif()
 
-  # Independent target to make it possible to have new dependencies each build
-  add_library(libdeps INTERFACE)
+if(HIGHFIVE_GLIBCXX_ASSERTIONS)
+target_compile_definitions(HighFive INTERFACE -D_GLIBCXX_ASSERTIONS)
+endif()
 
-  if(HIGHFIVE_VERBOSE)
-    target_compile_definitions(libdeps INTERFACE -DHIGHFIVE_LOG_LEVEL=0)
-  endif()
+if(HIGHFIVE_SANITIZER)
+target_compile_options(HighFive INTERFACE -fsanitize=${HIGHFIVE_SANITIZER})
+target_link_options(HighFive INTERFACE -fsanitize=${HIGHFIVE_SANITIZER})
+endif()
 
-  if(HIGHFIVE_GLIBCXX_ASSERTIONS)
-    target_compile_definitions(libdeps INTERFACE -D_GLIBCXX_ASSERTIONS)
-  endif()
+# HDF5
+if(NOT DEFINED HDF5_C_LIBRARIES)
+set(HDF5_PREFER_PARALLEL ${HIGHFIVE_PARALLEL_HDF5})
+find_package(HDF5 REQUIRED)
+endif()
 
-  if(HIGHFIVE_SANITIZER)
-    target_compile_options(libdeps INTERFACE -fsanitize=${HIGHFIVE_SANITIZER})
-    target_link_options(libdeps INTERFACE -fsanitize=${HIGHFIVE_SANITIZER})
-  endif()
+if(HIGHFIVE_PARALLEL_HDF5 AND NOT HDF5_IS_PARALLEL)
+message(WARNING "Parallel HDF5 requested but libhdf5 doesnt support it")
+endif()
 
-  # HDF5
-  if(NOT DEFINED HDF5_C_LIBRARIES)
-    set(HDF5_PREFER_PARALLEL ${HIGHFIVE_PARALLEL_HDF5})
-    find_package(HDF5 REQUIRED)
-  endif()
+target_include_directories(HighFive SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
+target_link_libraries(HighFive INTERFACE ${HDF5_LIBRARIES})
+target_compile_definitions(HighFive INTERFACE ${HDF5_DEFINITIONS})
 
-  if(HIGHFIVE_PARALLEL_HDF5 AND NOT HDF5_IS_PARALLEL)
-    message(WARNING "Parallel HDF5 requested but libhdf5 doesnt support it")
-  endif()
+# Boost
+if(HIGHFIVE_USE_BOOST)
+if(NOT DEFINED Boost_NO_BOOST_CMAKE)
+# HighFive deactivated finding Boost via Boost's own CMake files
+# in Oct 2016 (commit '25627b085'). Likely to appease one cluster.
+# Boost's CMake support has since improved and likely this setting
+# isn't needed anymore. It is kept for backwards compatibility.
+# However, a rework of HighFive's CMake code should consider removing
+# this default. Hard coding this to true has been reported to cause
+# build failures.
+set(Boost_NO_BOOST_CMAKE TRUE)
+endif()
+find_package(Boost REQUIRED COMPONENTS system serialization)
+# Dont use imported targets yet, not avail before cmake 3.5
+target_include_directories(HighFive SYSTEM INTERFACE ${Boost_INCLUDE_DIR})
+target_compile_definitions(HighFive INTERFACE BOOST_ALL_NO_LIB H5_USE_BOOST)
+endif()
 
-  target_include_directories(libdeps SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
-  target_link_libraries(libdeps INTERFACE ${HDF5_LIBRARIES})
-  target_compile_definitions(libdeps INTERFACE ${HDF5_DEFINITIONS})
+# Half
+if(HIGHFIVE_USE_HALF_FLOAT)
+find_file(FOUND_HALF half.hpp)
+if (NOT FOUND_HALF)
+message(FATAL_ERROR "Half-precision floating-point support requested but file half.hpp not found")
+endif()
+target_compile_definitions(HighFive INTERFACE H5_USE_HALF_FLOAT)
+endif()
 
-  # Boost
-  if(HIGHFIVE_USE_BOOST)
-    if(NOT DEFINED Boost_NO_BOOST_CMAKE)
-      # HighFive deactivated finding Boost via Boost's own CMake files
-      # in Oct 2016 (commit '25627b085'). Likely to appease one cluster.
-      # Boost's CMake support has since improved and likely this setting
-      # isn't needed anymore. It is kept for backwards compatibility.
-      # However, a rework of HighFive's CMake code should consider removing
-      # this default. Hard coding this to true has been reported to cause
-      # build failures.
-      set(Boost_NO_BOOST_CMAKE TRUE)
-    endif()
-    find_package(Boost REQUIRED COMPONENTS system serialization)
-    # Dont use imported targets yet, not avail before cmake 3.5
-    target_include_directories(libdeps SYSTEM INTERFACE ${Boost_INCLUDE_DIR})
-    target_compile_definitions(libdeps INTERFACE BOOST_ALL_NO_LIB H5_USE_BOOST)
-  endif()
+# Eigen
+if(HIGHFIVE_USE_EIGEN)
+if (NOT EIGEN3_INCLUDE_DIRS)
+find_package(Eigen3 NO_MODULE)
+if(Eigen3_FOUND)
+message(STATUS "Found Eigen ${Eigen3_VERSION}: ${EIGEN3_INCLUDE_DIRS}")
+else()
+find_package(PkgConfig)
+pkg_check_modules(EIGEN3 REQUIRED eigen3)
+endif()
+endif()
+if (NOT EIGEN3_INCLUDE_DIRS)
+message(FATAL_ERROR "Eigen was requested but could not be found")
+endif()
+target_include_directories(HighFive SYSTEM INTERFACE ${EIGEN3_INCLUDE_DIRS})
+target_compile_definitions(HighFive INTERFACE H5_USE_EIGEN)
+endif()
 
-  # Half
-  if(HIGHFIVE_USE_HALF_FLOAT)
-    find_file(FOUND_HALF half.hpp)
-    if (NOT FOUND_HALF)
-      message(FATAL_ERROR "Half-precision floating-point support requested but file half.hpp not found")
-    endif()
-    target_compile_definitions(libdeps INTERFACE H5_USE_HALF_FLOAT)
-  endif()
+# xtensor
+if(HIGHFIVE_USE_XTENSOR)
+if (NOT xtensor_INCLUDE_DIRS)
+find_package(xtensor REQUIRED)
+endif()
+if (NOT xtl_INCLUDE_DIRS)
+find_package(xtl REQUIRED)
+endif()
+target_include_directories(HighFive SYSTEM INTERFACE ${xtensor_INCLUDE_DIRS} ${xtl_INCLUDE_DIRS})
+target_compile_definitions(HighFive INTERFACE H5_USE_XTENSOR)
+endif()
 
-  # Eigen
-  if(HIGHFIVE_USE_EIGEN)
-    if (NOT EIGEN3_INCLUDE_DIRS)
-      find_package(Eigen3 NO_MODULE)
-      if(Eigen3_FOUND)
-        message(STATUS "Found Eigen ${Eigen3_VERSION}: ${EIGEN3_INCLUDE_DIRS}")
-      else()
-        find_package(PkgConfig)
-        pkg_check_modules(EIGEN3 REQUIRED eigen3)
-      endif()
-    endif()
-    if (NOT EIGEN3_INCLUDE_DIRS)
-      message(FATAL_ERROR "Eigen was requested but could not be found")
-    endif()
-    target_include_directories(libdeps SYSTEM INTERFACE ${EIGEN3_INCLUDE_DIRS})
-    target_compile_definitions(libdeps INTERFACE H5_USE_EIGEN)
-  endif()
+# OpenCV
+if(HIGHFIVE_USE_OPENCV)
+if (NOT OpenCV_INCLUDE_DIRS)
+find_package(OpenCV REQUIRED)
+endif()
+target_include_directories(HighFive SYSTEM INTERFACE ${OpenCV_INCLUDE_DIRS})
+target_link_libraries(HighFive INTERFACE ${OpenCV_LIBS})
+target_compile_definitions(HighFive INTERFACE H5_USE_OPENCV)
+endif()
 
-  # xtensor
-  if(HIGHFIVE_USE_XTENSOR)
-    if (NOT xtensor_INCLUDE_DIRS)
-      find_package(xtensor REQUIRED)
-    endif()
-    if (NOT xtl_INCLUDE_DIRS)
-      find_package(xtl REQUIRED)
-    endif()
-    target_include_directories(libdeps SYSTEM INTERFACE ${xtensor_INCLUDE_DIRS} ${xtl_INCLUDE_DIRS})
-    target_compile_definitions(libdeps INTERFACE H5_USE_XTENSOR)
-  endif()
-
-  # OpenCV
-  if(HIGHFIVE_USE_OPENCV)
-    if (NOT OpenCV_INCLUDE_DIRS)
-      find_package(OpenCV REQUIRED)
-    endif()
-    target_include_directories(libdeps SYSTEM INTERFACE ${OpenCV_INCLUDE_DIRS})
-    target_link_libraries(libdeps INTERFACE ${OpenCV_LIBS})
-    target_compile_definitions(libdeps INTERFACE H5_USE_OPENCV)
-  endif()
-
-  # MPI
-  if(HIGHFIVE_PARALLEL_HDF5 OR HDF5_IS_PARALLEL)
-    find_package(MPI REQUIRED)
-    target_include_directories(libdeps SYSTEM INTERFACE ${MPI_CXX_INCLUDE_PATH})
-    target_link_libraries(libdeps INTERFACE ${MPI_CXX_LIBRARIES})
-    if(CMAKE_VERSION VERSION_LESS 3.13)
-      target_link_libraries(libdeps INTERFACE ${MPI_CXX_LINK_FLAGS})
-    else()
-      target_link_options(libdeps INTERFACE "SHELL:${MPI_CXX_LINK_FLAGS}")
-    endif()
-  endif()
-
+# MPI
+if(HIGHFIVE_PARALLEL_HDF5 OR HDF5_IS_PARALLEL)
+find_package(MPI REQUIRED)
+target_include_directories(HighFive SYSTEM INTERFACE ${MPI_CXX_INCLUDE_PATH})
+target_link_libraries(HighFive INTERFACE ${MPI_CXX_LIBRARIES})
+if(CMAKE_VERSION VERSION_LESS 3.13)
+target_link_libraries(HighFive INTERFACE ${MPI_CXX_LINK_FLAGS})
+else()
+target_link_options(HighFive INTERFACE "SHELL:${MPI_CXX_LINK_FLAGS}")
+endif()
 endif()
