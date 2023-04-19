c6fa2ecbd5011ba983c41e6e7ad0067331820ab0
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-04-19 21:32:05 +0200

    Remove Eigen functionality for matrices.
    HighFive hasn't been respecting that Eigen uses Fortran order, as a
    result it's been writing `Eigen::Matrix` to file as if the data were C
    order. It's been consistently reading datasets back into `Eigen::Matrix`
    has if it already was in Fortran order.
    
    This error is symmetric, i.e., if one writes an `Eigen::Matrix` to disk
    via core HighFive and then reads that dataset back into an
    `Eigen::Matrix` the outcome is correct (which has been check in CI).
    
    More information can be found at:
    https://github.com/BlueBrain/HighFive/issues/532

--- include/highfive/bits/H5Converter_misc.hpp
+++ include/highfive/bits/H5Converter_misc.hpp
@@ -625,7 +625,18 @@ struct inspector<Eigen::Matrix<T, M, N>> {
     static constexpr bool is_trivially_copyable = std::is_trivially_copyable<value_type>::value &&
                                                   inspector<value_type>::is_trivially_copyable;
 
+
+    static void assert_not_buggy(Eigen::Index nrows, Eigen::Index ncols) {
+        if (nrows > 1 && ncols > 1) {
+            throw std::runtime_error(
+                "HighFive has been broken for Eigen::Matrix. Please check "
+                "https://github.com/BlueBrain/HighFive/issues/532.");
+        }
+    }
+
     static std::vector<size_t> getDimensions(const type& val) {
+        assert_not_buggy(val.rows(), val.cols());
+
         std::vector<size_t> sizes{static_cast<size_t>(val.rows()), static_cast<size_t>(val.cols())};
         auto s = inspector<value_type>::getDimensions(val.data()[0]);
         sizes.insert(sizes.end(), s.begin(), s.end());
@@ -646,23 +657,29 @@ struct inspector<Eigen::Matrix<T, M, N>> {
             val.resize(static_cast<typename type::Index>(dims[0]),
                        static_cast<typename type::Index>(dims[1]));
         }
+
+        assert_not_buggy(val.rows(), val.cols());
     }
 
     static hdf5_type* data(type& val) {
+        assert_not_buggy(val.rows(), val.cols());
         return inspector<value_type>::data(*val.data());
     }
 
     static const hdf5_type* data(const type& val) {
+        assert_not_buggy(val.rows(), val.cols());
         return inspector<value_type>::data(*val.data());
     }
 
     static void serialize(const type& val, hdf5_type* m) {
+        assert_not_buggy(val.rows(), val.cols());
         std::memcpy(m, val.data(), static_cast<size_t>(val.size()) * sizeof(hdf5_type));
     }
 
     static void unserialize(const hdf5_type* vec_align,
                             const std::vector<size_t>& dims,
                             type& val) {
+        assert_not_buggy(val.rows(), val.cols());
         if (dims.size() < 2) {
             std::ostringstream os;
             os << "Impossible to pair DataSet with " << dims.size()
