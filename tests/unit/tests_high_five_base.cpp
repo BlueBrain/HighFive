54377299551e07a11d5824e1fb208cccaa407846
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-11-30 11:58:36 +0100

    Use `-Werror` during CI.

--- tests/unit/tests_high_five_base.cpp
+++ tests/unit/tests_high_five_base.cpp
@@ -19,6 +19,7 @@
 #include <type_traits>
 #include <vector>
 
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_vector.hpp>
