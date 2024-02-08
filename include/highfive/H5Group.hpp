088e614d8a48f0a09987e30e7820c31679804fb2
Nicolas Cornu <nicolas.cornu@epfl.ch>
2024-02-08 17:18:52 +0100

    Remove default ctor for Group

--- include/highfive/H5Group.hpp
+++ include/highfive/H5Group.hpp
@@ -47,9 +47,7 @@ class Group: public Object,
   public:
     const static ObjectType type = ObjectType::Group;
 
-    /// \deprecated Default constructor creates unsafe uninitialized objects
-    H5_DEPRECATED("Default constructor creates unsafe uninitialized objects")
-    Group() = default;
+    Group() = delete;
 
     std::pair<unsigned int, unsigned int> getEstimatedLinkInfo() const;
 
