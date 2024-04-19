2dfc9c5104e1218a7a3c72d587de648e8c57394d
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-04-19 17:24:25 +0200

    Maybe like this.

--- include/highfive/boost.hpp
+++ include/highfive/boost.hpp
@@ -51,7 +51,7 @@ struct inspector<boost::multi_array<T, Dims>> {
         }
     }
 
-    static void assert_c_order(type& val) {
+    static void assert_c_order(const type& val) {
         if (!(val.storage_order() == boost::c_storage_order())) {
             throw DataTypeException("Only C storage order is supported for 'boost::multi_array'.");
         }
