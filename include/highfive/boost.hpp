e55cee113e1a326b6086db7ed8da13623270c86e
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-04-19 16:19:36 +0200

    Add `inspector::assert_c_order`.

--- include/highfive/boost.hpp
+++ include/highfive/boost.hpp
@@ -51,28 +51,26 @@ struct inspector<boost::multi_array<T, Dims>> {
         }
     }
 
-    static hdf5_type* data(type& val) {
+    static void assert_c_order(type& val) {
         if (!(val.storage_order() == boost::c_storage_order())) {
             throw DataTypeException(
-                "Only C Storage Order is supported for boost::multi_array() now.");
+                "Only C storage order is supported for 'boost::multi_array'.");
         }
+    }
+
+    static hdf5_type* data(type& val) {
+        assert_c_order(val);
         return inspector<value_type>::data(*val.data());
     }
 
     static const hdf5_type* data(const type& val) {
-        if (!(val.storage_order() == boost::c_storage_order())) {
-            throw DataTypeException(
-                "Only C Storage Order is supported for boost::multi_array() now.");
-        }
+        assert_c_order(val);
         return inspector<value_type>::data(*val.data());
     }
 
     template <class It>
     static void serialize(const type& val, const std::vector<size_t>& dims, It m) {
-        if (!(val.storage_order() == boost::c_storage_order())) {
-            throw DataTypeException(
-                "Only C Storage Order is supported for boost::multi_array() now.");
-        }
+        assert_c_order(val);
         size_t size = val.num_elements();
         auto subdims = std::vector<size_t>(dims.begin() + ndim, dims.end());
         size_t subsize = compute_total_size(subdims);
@@ -83,10 +81,7 @@ struct inspector<boost::multi_array<T, Dims>> {
 
     template <class It>
     static void unserialize(It vec_align, const std::vector<size_t>& dims, type& val) {
-        if (!(val.storage_order() == boost::c_storage_order())) {
-            throw DataTypeException(
-                "Only C Storage Order is supported for boost::multi_array() now.");
-        }
+        assert_c_order(val);
         std::vector<size_t> next_dims(dims.begin() + ndim, dims.end());
         size_t subsize = compute_total_size(next_dims);
         for (size_t i = 0; i < val.num_elements(); ++i) {
