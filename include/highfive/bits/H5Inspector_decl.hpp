fcec8d6c20f24fe6a4056a332546de517dbc6581
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-05-01 20:08:54 +0200

    Move `detail::compute_total_size` to separate file.

--- include/highfive/bits/H5Inspector_decl.hpp
+++ include/highfive/bits/H5Inspector_decl.hpp
@@ -1,19 +1,11 @@
 #pragma once
 
-#include <cstddef>
-#include <numeric>
-#include <functional>
-#include <vector>
+#include "compute_total_size.hpp"
 
 namespace HighFive {
 
-inline size_t compute_total_size(const std::vector<size_t>& dims) {
-    return std::accumulate(dims.begin(), dims.end(), size_t{1u}, std::multiplies<size_t>());
-}
-
 template <typename T>
 using unqualified_t = typename std::remove_const<typename std::remove_reference<T>::type>::type;
-
 
 namespace details {
 
