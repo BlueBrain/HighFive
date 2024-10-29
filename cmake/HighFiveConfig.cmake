5eea4e52ec131c06760910fbe837d669223fe313
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-10-29 19:32:04 +0100

    Guard targets in `HighFiveConfig.cmake`.

--- cmake/HighFiveConfig.cmake
+++ cmake/HighFiveConfig.cmake
@@ -8,12 +8,16 @@ if(HIGHFIVE_FIND_HDF5)
   find_dependency(HDF5)
 endif()
 
-include("${CMAKE_CURRENT_LIST_DIR}/HighFiveTargets.cmake")
+if(NOT TARGET HighFive)
+  include("${CMAKE_CURRENT_LIST_DIR}/HighFiveTargets.cmake")
 
-if(HDF5_IS_PARALLEL)
-  find_dependency(MPI)
-  target_link_libraries(HighFive::HighFive INTERFACE MPI::MPI_C MPI::MPI_CXX)
+  if(HDF5_IS_PARALLEL)
+    find_dependency(MPI)
+    target_link_libraries(HighFive::HighFive INTERFACE MPI::MPI_C MPI::MPI_CXX)
+  endif()
+
+  add_library(HighFive ALIAS HighFive::HighFive)
+  add_library(HighFiveInclude ALIAS HighFive::Include)
 endif()
 
-add_library(HighFive ALIAS HighFive::HighFive)
-add_library(HighFiveInclude ALIAS HighFive::Include)
+
