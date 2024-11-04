018103c382c1d883a97fa22971cf6998514881e4
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-11-04 10:49:26 +0100

    Error on mismatching formatter.

--- bin/format.sh
+++ bin/format.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+clang_format_version="19.1.3"
+
 script_dir="$(dirname "${BASH_SOURCE[0]}")"
 venv_dir="$script_dir/../.clang-format-venv"
 
 if [ ! -d "$venv_dir" ]; then
     python3 -m venv "$venv_dir"
     source "$venv_dir/bin/activate"
-    pip install clang-format==19.1.3
+    pip install clang-format=="$clang_format_version"
     deactivate
 fi
 
 source "$venv_dir/bin/activate"
+
+# Check if the installed version matches the expected version
+installed_version="$(pip show clang-format | grep Version | cut -d ' ' -f 2)"
+
+if [ "$installed_version" != "$clang_format_version" ]; then
+    echo "Error: clang-format version mismatch. Expected $clang_format_version, got $installed_version"
+    echo "Please remove the virtual environment and run the script again:"
+    echo "    rm -r \"$venv_dir\" && \"$0\""
+    exit 1
+fi
 
 clang-format --version
 for i in $(git ls-files | grep ".[ch]pp$"); do
