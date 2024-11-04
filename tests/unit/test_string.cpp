61f515226293c936e67039f4a0ddaa0e1984598c
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-11-04 10:22:02 +0100

    Allow reading not-quite null-terminated strings. (#1056)
    For fixed-length strings that have a padding mode that suggests they're
    null-terminated are in fact not required to be null-terminated, we (silently)
    fail to read the last character. Since, HDF5 will create such string, h5dump
    will display the file and h5py will read the string; we opted to allow it and
    also read the string into and `std::string` (which is variable length and already
    not guaranteed to have the same length as the fixed length string).
    
    HighFive will continue to not allow writing null-terminated strings that aren't
    via `write`.

--- tests/unit/test_string.cpp
+++ tests/unit/test_string.cpp
@@ -206,6 +206,33 @@ void check_multiple_string(File file, size_t string_length) {
     }
 }
 
+template <class CreateTraits>
+void check_supposedly_nullterm(HighFive::File& file) {
+    auto dataspace = HighFive::DataSpace::Scalar();
+    auto datatype = HighFive::FixedLengthStringType(5, HighFive::StringPadding::NullTerminated);
+    auto obj = CreateTraits::create(file, "not_null_terminated", dataspace, datatype);
+
+    // Creates a 5 byte, "null-terminated", fixed-length string. The first five
+    // bytes are filled with "GROUP". Clearly, this isn't null-terminated. However,
+    // h5py will read it back as "GROUP", HDF5 allows us to create these; and they're
+    // found in the wild.
+    std::string value = "GROUP";
+    obj.write_raw(value.c_str(), datatype);
+
+    auto actual = obj.template read<std::string>();
+    REQUIRE(actual == value);
+}
+
+TEST_CASE("HighFiveSTDString (attribute, nullterm cornercase)") {
+    auto file = HighFive::File("not_null_terminated_attribute.h5", HighFive::File::Truncate);
+    check_supposedly_nullterm<testing::AttributeCreateTraits>(file);
+}
+
+TEST_CASE("HighFiveSTDString (dataset, nullterm cornercase)") {
+    auto file = HighFive::File("not_null_terminated_dataset.h5", HighFive::File::Truncate);
+    check_supposedly_nullterm<testing::DataSetCreateTraits>(file);
+}
+
 TEST_CASE("HighFiveSTDString (dataset, single, short)") {
     File file("std_string_dataset_single_short.h5", File::Truncate);
     check_single_string<testing::DataSetCreateTraits>(file, 3);
