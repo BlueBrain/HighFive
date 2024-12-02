c890ad4b8bda7a78fc3c80e78fbecc6c566991e8
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-12-02 12:48:52 +0100

    Run parallel examples during CI. (#1063)

--- src/examples/parallel_hdf5_collective_io.cpp
+++ src/examples/parallel_hdf5_collective_io.cpp
@@ -92,7 +92,7 @@ int main(int argc, char** argv) {
         std::vector<size_t> offset{std::size_t(mpi_rank), 0ul};
         std::vector<size_t> count{1ul, 2ul};
 
-        dataset.select(offset, count).write(data, xfer_props);
+        dataset.select(offset, count).squeezeMemSpace({0}).write(data, xfer_props);
         check_collective_io(xfer_props);
 
         // Let's ensure that everything has been written do disk.
@@ -104,7 +104,7 @@ int main(int argc, char** argv) {
 
         // MPI ranks don't have to read non-overlapping parts, but in this
         // example they happen to. Again all rank participate in this call.
-        dataset.select(offset, count).read(data, xfer_props);
+        dataset.select(offset, count).squeezeMemSpace({0}).read(data, xfer_props);
         check_collective_io(xfer_props);
 
     } catch (Exception& err) {
